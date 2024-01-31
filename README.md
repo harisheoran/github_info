ccb868a1e487c7320a2707740bd0aac450f2511f
Harish Sheoran <harisheoran01@gmail.com>
2024-01-31 16:51:14 +0530

    added flags

---
+++ README.md
@@ -0,0 +1,16 @@
+A simple CLI tool to get list of collaborators in a Github repo.
+
+## How to use
+- First, provide GitHub API token
+```
+export TOKEN=<your token here>
+```
+- ```team``` command to get list of collaborators
+- Use flag --u for providing username of GitHub
+- Use flag --r for providing GitHub repository name
+
+example
+```
+team --u=harisheoran --r=collegehub
+```
+
--- cmd/team.go
+++ cmd/team.go
@@ -2,12 +2,12 @@ package cmd
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
 	"os"
 
 	"github.com/google/go-github/v58/github"
-	"github.com/joho/godotenv"
 	"github.com/spf13/cobra"
 )
 
@@ -16,28 +16,33 @@ var teamCmd = &cobra.Command{
 	Use:   "team",
 	Short: "Get list of Collaborators of Github Repo",
 	Run: func(cmd *cobra.Command, args []string) {
-		username, err := cmd.Flags().GetString("u")
-		repo, err1 := cmd.Flags().GetString("r")
+		_, err := getEnv()
 		if err != nil {
-			log.Fatal("Username flag error", err)
-		}
-		if err1 != nil {
-			log.Fatal("Username flag error", err)
-		}
-
-		if username == "" {
-			fmt.Println("No username. Provide Username: --u=harish")
-		} else if repo == "" {
-			fmt.Println("No username. Provide Repository: --r=kubernetes")
+			fmt.Println(err)
 		} else {
-			getCollaborators(username, repo)
+			username, err := cmd.Flags().GetString("u")
+			repo, err1 := cmd.Flags().GetString("r")
+			if err != nil {
+				log.Fatal("Username flag error", err)
+			}
+			if err1 != nil {
+				log.Fatal("Username flag error", err)
+			}
+
+			if username == "" {
+				fmt.Println("No username. Provide Username: --u=harish")
+			} else if repo == "" {
+				fmt.Println("No username. Provide Repository: --r=kubernetes")
+			} else {
+				getCollaborators(username, repo)
+			}
 		}
 	},
 }
 
 // Prints the list of all collaborators
 func getCollaborators(username, repo string) {
-	token := getEnv("TOKEN")
+	token, _ := getEnv()
 	list := getCollaboratorsList(token, username, repo)
 	for i := 0; i < len(list); i++ {
 		fmt.Printf("%d. Name: %s Access: %s\n", i, *list[i].Login, *list[i].RoleName)
@@ -53,21 +58,21 @@ func getCollaboratorsList(token, username, repo string) []*github.User {
 	response, _, err := client.Repositories.ListCollaborators(ctx, username, repo, &github.ListCollaboratorsOptions{})
 
 	if err != nil {
-		log.Print("Error is ", err)
+		log.Print("Please provide correct username and repository name ", err)
 	}
 
 	return response
 }
 
 // Load the .env file and return the variable
-func getEnv(key string) string {
-	err := godotenv.Load(".env")
+func getEnv() (string, error) {
+	token := os.Getenv("TOKEN")
 
-	if err != nil {
-		log.Fatal("Env error ", err)
+	if len(token) == 0 {
+		return "", errors.New("Need Github Token. Provide using: export TOKEN=\"<your-token>\"")
 	}
 
-	return os.Getenv(key)
+	return token, nil
 }
 
 func init() {
